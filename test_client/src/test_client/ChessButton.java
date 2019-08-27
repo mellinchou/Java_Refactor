0f8c1685a75498b281d71675f3570b7a29fd0957
JIMBOO <jim4809jim4809@gmail.com>
2019-08-27 16:59:42 +0800

    Delete ChessButtonListener

--- test_client/src/test_client/ChessButton.java
+++ test_client/src/test_client/ChessButton.java
@@ -1,25 +1,28 @@
 package test_client;
 
-
 import javax.swing.JButton;
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
 //this is the individual buttons on the chess board
-public class ChessButton extends JButton{
+public class ChessButton extends JButton implements ActionListener {
 	private int x;
 	private int y;
 	private String coordinates;
-	
-	public ChessButton(int x,int y) { //when creating the button object, use x and y to record it's coordinates
+
+	public ChessButton(int x, int y) { // when creating the button object, use x and y to record it's coordinates
 		this.addActionListener(new ChessButtonListener());
-		this.x=x;
-		this.y=y;
+		this.x = x;
+		this.y = y;
 	}
-	
+
 	public String getCoordinates() {
 		return coordinates;
 	}
-	
-	public String getName() { //to return the coordinates recorded
+
+	public String getName() { // to return the coordinates recorded
 		StringBuilder stringBuilder = new StringBuilder();
 
 		stringBuilder.append(this.y);
@@ -27,5 +30,24 @@ public class ChessButton extends JButton{
 		stringBuilder.append(this.x);
 		String coordinates = stringBuilder.toString();
 		return coordinates;
+	}
+
+	public String coordinates = "";
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		((Component) e.getSource()).setEnabled(false);
+
+		coordinates = ((Component) e.getSource()).getName();// get the coordinates of the clicked button
+		String[] cor = coordinates.split(" ");// split the string into separate numbers
+
+		try {
+			Main.send(Integer.parseInt(cor[0]), Integer.parseInt(cor[1]));// send the coordinates back to the server
+		} catch (NumberFormatException e1) {
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+
 	}
 }
